8b214e7bca7c7a47b765bba016e5a95867075e48
flar2 <asegaert@gmail.com>
2020-10-16 17:14:14 +0200

    Remove ALWAYS_ENFORCE.
    Change-Id: Ib27655280a2ca0fbdbc8dfa1d89a3a8aba7fac51

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -759,17 +759,8 @@ out:
 	kfree(n);
 	kfree(t);
 
-// [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
-#if (defined CONFIG_KDP_CRED && defined CONFIG_SAMSUNG_PRODUCT_SHIP)
-	enforcing_set(NULL, 1);
-#else
-	selinux_enforcing = 1;
-#endif
-#endif
 	if (!selinux_enforcing) // SEC_SELINUX_PORTING_COMMON Change to use RKP 
 		return 0;
-// ] SEC_SELINUX_PORTING_COMMON
 	return -EPERM;
 }
 
@@ -1613,17 +1604,8 @@ out:
 	kfree(t);
 	kfree(n);
 
-// [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
-#if (defined CONFIG_KDP_CRED && defined CONFIG_SAMSUNG_PRODUCT_SHIP)
-	enforcing_set(NULL, 1);
-#else
-	selinux_enforcing = 1;
-#endif
-#endif
 	if (!selinux_enforcing) // SEC_SELINUX_PORTING_COMMON Change to use RKP 
 		return 0;
-// ] SEC_SELINUX_PORTING_COMMON
 	return -EACCES;
 }
 
@@ -1933,17 +1915,8 @@ static inline int convert_context_handle_invalid_context(
 	char *s;
 	u32 len;
 
-// [ SEC_SELINUX_PORTING_COMMON 
-#ifdef CONFIG_ALWAYS_ENFORCE
-#if (defined CONFIG_KDP_CRED && defined CONFIG_SAMSUNG_PRODUCT_SHIP)
-	enforcing_set(NULL, 1);
-#else
-	selinux_enforcing = 1;
-#endif
-#endif
 	if (!selinux_enforcing) // SEC_SELINUX_PORTING_COMMON Change to use RKP
 		return -EINVAL;
-// ] SEC_SELINUX_PORTING_COMMON
 
 	if (!context_struct_to_string(policydb, context, &s, &len)) {
 		pr_warn("SELinux:  Context %s would be invalid if enforcing\n",
